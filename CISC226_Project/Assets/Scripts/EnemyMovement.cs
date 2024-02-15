5a26f2866595b977b614077ad2bd2539ddc8c3a9
clairewhelan <145299572+clairewhelan@users.noreply.github.com>
2024-02-15 15:55:40 -0500

    basic enemy following works
    I just need to update speed + trigger end of game when player touches it + have bat route around enemy on diagonals so players dont kill themselves accidentally

---
+++ CISC226_Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public OverlayTile onTile;
+    
+    public float speed;
+    OverlayTile target;
+    bool isMoving = false;
+
+    private PathFinder pathFinder;
+    //private List<OverlayTile> path = new List<OverlayTile>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathFinder = new PathFinder();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //triggered in ApproachPlayer method below, called from MouseController
+        if (isMoving)
+        {
+            
+            MoveToTile(target);
+
+            if (transform.position == target.gridLocation)
+            {
+                isMoving = false;
+            }
+        }
+
+        
+    }
+
+    //called by MouseController on rightclick
+    public void ApproachPlayer(OverlayTile enemyTile, OverlayTile playerTile)
+    {
+
+        List<OverlayTile> path = pathFinder.FindPath(enemyTile, playerTile);
+        target = path[0];
+
+        isMoving = true;
+        
+    }
+
+    //altered version of the one in MouseController
+    private void MoveToTile(OverlayTile tile)
+    {
+        var step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, tile.transform.position, step);
+
+        if(Vector2.Distance(transform.position, tile.transform.position) < 0.0001f){
+            PositionEnemyOnTile(tile);
+        }
+    }
+
+    //also altered verson of the one in MouseController
+    private void PositionEnemyOnTile(OverlayTile tile)
+    {
+        transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y, tile.transform.position.z);
+        GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
+        onTile = tile;
+    }
+    
+
+}
