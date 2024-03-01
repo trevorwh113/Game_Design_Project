97a6c8b174bfd1635ce01291a6f059d8c3c526c5
clairewhelan <145299572+clairewhelan@users.noreply.github.com>
2024-03-01 15:26:15 -0500

    coins basic system
    -created Coin and CoinSparkle objects
    -coin & sparkle use sprite renderers which are toggled when player moves to the same square, making them disappear
    -number of coins collected is printed to console

---
+++ CISC226_Project/Assets/Scripts/CoinSparkleInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSparkleInfo : MonoBehaviour
+{
+    // public OverlayTile onTile;
+
+    public SpriteRenderer spriteRenderer;
+
+}
