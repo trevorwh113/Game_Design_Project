eb14442517076776400243b8f44c194379aa6109
trevorwh113 <trevor.wh113@gmail.com>
2024-03-08 21:02:43 -0500

    Shop!
    - Shop feature has been implemented.
    - It is functional.
    - It works.
    - Assets to come at a later date.

---
+++ CISC226_Project/Assets/Scripts/Shop/ItemInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemInfo
+{
+    // The item name.
+    public string name;
+
+    // The item cost.
+    public int cost;
+
+
+    // The item description.
+    public string description;
+
+    // The image to display with the item.
+    public Sprite icon;
+
+}
